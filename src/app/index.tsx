1ee9c014e3c4ac25a922228df23126e25f24e438
yuya21011111 <kaimomo12345@gmail.com>
2025-04-21 02:34:53 +0900

    Test

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const Index = (): JSX.Element => {
+    return (
+        <View>
+            <Text>Hello Word</Text>
+        </View>
+    )
+}
+
+export default Index
