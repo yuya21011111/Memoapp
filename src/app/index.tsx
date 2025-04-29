1c39b5d1bf669b3b33185fa6cb87c04d9f3be5f2
yuya21011111 <kaimomo12345@gmail.com>
2025-04-29 23:06:55 +0900

    component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,45 +1,14 @@
 import { View, Text, StyleSheet } from 'react-native'
+import Header from '../components/Header'
+import MemoListItem from '../components/MemoListItem'
 
 const Index = (): JSX.Element => {
     return (
         <View style={styles.container}>
-            <View  style={styles.header}>
-                <View style={styles.headerInner}>
-                    <Text style={styles.headerTitle}>Memo App</Text>
-                    <Text style={styles.headerRight}>ログアウト</Text>
-                </View>
-            </View>
+            <Header />
 
             <View>
-                <View style={styles.memoListItem}>
-                    <View>
-                        <View style={styles.memoListItemTitle}>買い物リスト</View>
-                        <View style={styles.memoListItemDate}>2025年4月26日 15:00</View>
-                    </View>
-                    <View>
-                        <View>X</View>
-                    </View>
-                </View>
-
-                <View style={styles.memoListItem}>
-                    <View>
-                        <View style={styles.memoListItemTitle}>買い物リスト</View>
-                        <View style={styles.memoListItemDate}>2025年4月26日 15:00</View>
-                    </View>
-                    <View>
-                        <View>X</View>
-                    </View>
-                </View>
-
-                <View style={styles.memoListItem}>
-                    <View>
-                        <View style={styles.memoListItemTitle}>買い物リスト</View>
-                        <View style={styles.memoListItemDate}>2025年4月26日 15:00</View>
-                    </View>
-                    <View>
-                        <View>X</View>
-                    </View>
-                </View>
+               <MemoListItem />
             </View>
 
             <View style={styles.circleButton}>
@@ -53,47 +22,6 @@ const styles = StyleSheet.create({
     container:{
         flex: 1,
         backgroundColor: '#ffffff'
-    },
-    header: {
-        backgroundColor: '#467FD3',
-        height: 104,
-        justifyContent: 'flex-end'
-    },
-    headerInner: {
-        alignItems: 'center',
-    },
-    headerRight: {
-        position: 'absolute',
-        right: 16,
-        bottom: 16,
-        color: 'rgba(255,255,255,0.7)'
-    },
-    headerTitle: {
-        marginBottom: 8,
-        fontSize: 22,
-        lineHeight: 32,
-        fontWeight: 'bold',
-        color: '#ffffff'
-    },
-    memoListItem: {
-        backgroundColor: '#ffffff',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingVertical: 16,
-        paddingHorizontal: 19,
-        alignItems: 'center',
-        borderBottomWidth: 1,
-        borderColor: 'rgba(0,0,0,0.15)'
-    },
-    memoListItemTitle: {
-        fontSize: 16,
-        lineHeight: 32
-    },
-
-    memoListItemDate: {
-        fontSize: 12,
-        lineHeight: 16,
-        color: '#848484'
     },
     circleButton: {
         width: 64,
---
+++ src/components/MemoListItem.tsx
@@ -0,0 +1,40 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+const MemoListItem = (): JSX.Element => {
+    return (
+        <View style={styles.memoListItem}>
+            <View>
+                <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+                <Text style={styles.memoListItemDate}>2025年4月26日 15:00</Text>
+            </View>
+            <View>
+                <View>X</View>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    memoListItem: {
+        backgroundColor: '#ffffff',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 16,
+        paddingHorizontal: 19,
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        borderColor: 'rgba(0,0,0,0.15)'
+    },
+    memoListItemTitle: {
+        fontSize: 16,
+        lineHeight: 32
+    },
+
+    memoListItemDate: {
+        fontSize: 12,
+        lineHeight: 16,
+        color: '#848484'
+    },
+})
+
+export default MemoListItem
