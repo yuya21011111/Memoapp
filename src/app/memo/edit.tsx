d0ea2ed8404f547fae1ae287d4804f3d87f74b46
yuya21011111 <kaimomo12345@gmail.com>
2025-05-06 03:34:21 +0900

    Edit.tsx

--- src/app/memo/edit.tsx
+++ src/app/memo/edit.tsx
@@ -1,9 +1,37 @@
-import { View, Text, ScrollView, StyleSheet } from 'react-native'
+import { View, TextInput, ScrollView, StyleSheet } from 'react-native'
+import Header from '../../components/Header'
+import CircleButton from '../../components/CircleButton'
+import Icon from '../../components/icon'
 
 const Edit = (): JSX.Element => {
     return (
-        <View></View>
+        <View style={styles.container}>
+            <Header />
+            <View style={styles.inputContainer}>
+                <TextInput multiline style={styles.input} value={ '買い物\nリスト' } />
+            </View>
+            <CircleButton>
+                <Icon name='check' size={40} color='#ffffff' />
+            </CircleButton> 
+        </View>
     )
 }
 
 export default Edit
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    inputContainer: {
+        paddingVertical: 32,
+        paddingHorizontal: 27,
+        flex: 1,
+    },
+    input: {
+        flex: 1,
+        textAlignVertical: 'top',
+        fontSize: 16,
+        lineHeight: 24
+    }
+})
