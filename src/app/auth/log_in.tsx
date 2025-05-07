880db8d82ced88c6540fef24b40c67df69a43409
yuya21011111 <kaimomo12345@gmail.com>
2025-05-07 21:57:51 +0900

    'log_in.tsx'

--- src/app/auth/log_in.tsx
+++ src/app/auth/log_in.tsx
@@ -5,16 +5,16 @@ const LogIn = (): JSX.Element => {
     return (
         <View style={styles.container}>
             <Header />
-            <View>
-                <Text>Log In</Text>
-                <TextInput value='Email Address' />
-                <TextInput value='PassWord' />
-                <View>
-                    <Text>Submit</Text>
+            <View style={styles.inner}>
+                <Text style={styles.title}>Log In</Text>
+                <TextInput style={styles.input} value='Email Address' />
+                <TextInput style={styles.input} value='PassWord' />
+                <View style={styles.button}>
+                    <Text style={styles.buttonLabel}>Submit</Text>
                 </View>
-                <View>
-                    <Text>Not registered??</Text>
-                    <Text>Sign up here!</Text>
+                <View style={styles.footer}>
+                    <Text style={styles.footerText}>Not registered??</Text>
+                    <Text style={styles.footerLink}>Sign up here!</Text>
                 </View>
             </View>
         </View>
@@ -23,7 +23,55 @@ const LogIn = (): JSX.Element => {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        backgroundColor: '#F0F4F8'
+    },
+    title: {
+        fontSize: 24,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        marginBottom: 24
+    },
+    inner: {
+        paddingVertical: 24,
+        paddingHorizontal: 27
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#DDDDDD',
+        backgroundColor: '#ffffff',
+        height: 48,
+        padding: 8,
+        fontSize: 16,
+        marginBottom: 16
+    },
+    button: {
+        backgroundColor: '#467FD3',
+        borderRadius: 4,
+        alignSelf: 'flex-start',
+        marginBottom: 24,
+    },
+    buttonLabel: {
+        fontSize: 16,
+        lineHeight: 32,
+        color: '#ffffff',
+        paddingVertical: 8,
+        paddingHorizontal: 24,
+    },
+    footer: {
+        flexDirection: 'row',
+    },
+    footerText: {
+        fontSize: 14,
+        lineHeight: 24,
+        marginRight: 8,
+        color: '#000000'
+    },
+    footerLink: {
+        fontSize: 14,
+        lineHeight: 24,
+       color: '#467FD3'
     }
+
 }) 
 export default LogIn
