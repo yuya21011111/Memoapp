3cd35ad84142c33df1de46aa9d8325ffad15b87e
yuya21011111 <kaimomo12345@gmail.com>
2025-05-10 20:35:02 +0900

    button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,15 @@
-import { View, Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, TouchableOpacity } from "react-native";
 
 interface Props {
-    label: String
+    label: String,
+    onPress?: () => void
 }
 const Button = (props: Props): JSX.Element => {
-    const { label } = props
+    const { label, onPress } = props
     return (
-        <View style={styles.button}>
+        <TouchableOpacity onPress={onPress} style={styles.button}>
             <Text style={styles.buttonLabel}>{label}</Text>
-        </View>
+        </TouchableOpacity>
     )
 }
 
