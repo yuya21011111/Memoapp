8a247428002d1081307c9d77b9e702c5a93bf661
yuya21011111 <kaimomo12345@gmail.com>
2025-05-25 19:51:39 +0900

    config.tsx

--- src/config.ts
+++ src/config.ts
@@ -3,14 +3,7 @@ import { initializeAuth,  getReactNativePersistence } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import  ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyACU4YnSLDvMeyihP-_OkZljXRZFvVvR1g",
-  authDomain: "memoapp-bfdd5.firebaseapp.com",
-  projectId: "memoapp-bfdd5",
-  storageBucket: "memoapp-bfdd5.firebasestorage.app",
-  messagingSenderId: "643580564076",
-  appId: "1:643580564076:web:0dd9f62643ca2eeb05be49"
-};
+
 
 const app = initializeApp(firebaseConfig);
 const auth = initializeAuth(app, {
