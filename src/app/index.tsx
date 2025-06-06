960a9b944d1c4133ef2166e7bbbfc4283727cd42
yuya21011111 <kaimomo12345@gmail.com>
2025-06-06 23:44:47 +0900

    index

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,16 @@
-import { Redirect } from "expo-router";
+import { Redirect, router } from "expo-router";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../config";
+import { useEffect } from "react";
 
 const Index = (): JSX.Element => {
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if(user !== null) {
+                router.replace('/memo/list')
+            }
+        })
+    },[])
     return <Redirect href='auth/log_in' />
 }
 
